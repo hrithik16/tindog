7bbb962c4c3f8142efc3048cca469a73be4db6d7
Hrithik Gupta <hrithikguptagithub@gmail.com>
2026-05-13 13:28:30 +0530

    adding tracker

---
+++ tracker/tracker.js
@@ -0,0 +1,50 @@
+(() => {
+  const API_URL = "http://localhost:5000/api/events";
+
+  const getSessionId = () => {
+    let sessionId = localStorage.getItem("session_id");
+
+    if (!sessionId) {
+      sessionId = crypto.randomUUID();
+      localStorage.setItem("session_id", sessionId);
+    }
+
+    return sessionId;
+  };
+
+  const sendEvent = async (eventData) => {
+    try {
+      await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(eventData),
+      });
+    } catch (err) {
+      console.error("Tracking failed", err);
+    }
+  };
+
+  const basePayload = () => ({
+    session_id: getSessionId(),
+    page_url: window.location.href,
+    timestamp: new Date(),
+  });
+
+  // Page View
+  sendEvent({
+    ...basePayload(),
+    event_type: "page_view",
+  });
+
+  // Click Tracking
+  document.addEventListener("click", (e) => {
+    sendEvent({
+      ...basePayload(),
+      event_type: "click",
+      x: e.clientX,
+      y: e.clientY,
+    });
+  });
+})();
